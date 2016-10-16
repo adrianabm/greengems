f4668c41c5f69d877e9455831facc01e33fdbea7
adrianabm <adrianabm@gmail.com>
2016-10-16 19:09:23 +0200

    Add Shopping Cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
   get 'pages/home'
+
   root 'products#index'
 
   resources :profiles, only: [:new, :edit, :create, :update]
   resources :products, only: [:show, :index]
   resources :photos
-
+  resources :order_items
+  resource :cart, only: [:show]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
