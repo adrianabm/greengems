07ff6a8938363d8b0b3630e2906ae8f1d44b5048
adrianabm <adrianabm@gmail.com>
2016-10-16 10:01:38 +0200

    Create Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  has_one :profile
+
+  def has_profile?
+    profile.present?
+  end
+
+  def full_name
+    profile.full_name
+  end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   root to: 'pages#home'
 
+  resources :profiles, only: [:new, :edit, :create, :update]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
