672f07517fc6e9c0d0e4ed6bcebeb7416625c908
adrianabm <adrianabm@gmail.com>
2016-10-17 23:13:20 +0200

    Updated checkout button and validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,10 @@
 class Product < ApplicationRecord
   has_many :photos
   has_many :order_items
-
   default_scope { where(active: true) }
 
   def self.search(search)
     Product.where('name ILIKE :search OR description ILIKE :search', search: "%#{search}%")
   end
-
 
 end
