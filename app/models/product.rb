bfea000f690603dfe42c62a3152a6f89e76130ce
annedien <annedient@gmail.com>
2016-10-18 09:04:11 +0200

    Added product.rb and removed product.rb.orig from git

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@ class Product < ApplicationRecord
 
   default_scope { where(active: true) }
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
   def self.search(search)
     Product.where('name ILIKE :search OR description ILIKE :search', search: "%#{search}%")
   end
