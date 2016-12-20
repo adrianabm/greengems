2f32ed24078c9dd370ca289db4cc56b0487884de
Adriana Matsufuji <adrianabm@gmail.com>
2016-12-20 17:22:42 +0100

    update bundle and add thumbnail photos on index for plants

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :thumbnail do
     eager
-    resize_to_fit(500, 500)
+    resize_to_fit(200, 200)
     cloudinary_transformation :quality => 80
     end
 end
